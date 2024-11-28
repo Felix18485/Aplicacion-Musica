6a9d34147e0b6405fc9a5e94d674dad15542cbfc
Felix18485 <felix18485@gmail.com>
2024-11-28 19:49:14 +0100

    Funcionalidad post creada

--- script.js
+++ script.js
@@ -10,7 +10,51 @@ document.getElementById("btn-cerrar").addEventListener("click", () => {
 })
 
 //Evento que se encarga de ocultar el formulario al clickar fuera del formulario
-document.getElementById("form-container").addEventListener("blur", (event) => {
-    event.target.style.display = "none";
-})
+//document.getElementById("form-container").addEventListener("blur", (event) => {
+//    event.target.style.display = "none";
+//})
 
+async function postCancion() {
+    //Creamos un formData y lo llenamos con los archivos y textos necesarios
+    let inputCancion = document.getElementById("inputCancion");
+    let inputTitulo = document.getElementById("inputTitulo");
+    let inputArtista = document.getElementById("inputAutor");
+    let inputPortada = document.getElementById("inputImagen");
+    let formData = new FormData();
+    //El primer campo es la clave para enviarlo por post y el segundo el valor
+    formData.append("music", inputCancion.files[0]);
+    formData.append("title", inputTitulo.value);
+    formData.append("artist", inputArtista.value);
+    formData.append("cover", inputPortada.files[0]);
+
+    try {
+        const response = await fetch("http://informatica.iesalbarregas.com:7008/upload", {
+            method: "POST",
+            body: formData,
+        })
+        if (!response.ok) {
+            throw new Error("Error en la solicitud: " + response.statusText);
+        }
+        console.log("exito");
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getCanciones() {
+    try {
+        const response = await fetch("http://informatica.iesalbarregas.com:7008/songs");
+        if (!response.ok) {
+            throw new Error("Error en la solicitud: " + response.statusText);
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error");
+    }
+
+}
+
+document.getElementById("subir").addEventListener("click", postCancion);
+getCanciones();
